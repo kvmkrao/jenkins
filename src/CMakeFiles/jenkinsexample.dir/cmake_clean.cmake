ee13f3d2c2f5cab41008bdac271772fbed4a07d3
V Kotteda <kvmkrao@gmail.com>
2022-03-22 17:12:07 -0600

    updated

---
+++ src/CMakeFiles/jenkinsexample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jenkinsexample.dir/helloWorld.o"
+  "CMakeFiles/jenkinsexample.dir/main.o"
+  "jenkinsexample"
+  "jenkinsexample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jenkinsexample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
